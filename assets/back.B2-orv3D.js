a6af212587b1c7043dc2ea55b18feb3d2e8314eb
leostudiooo <leostudiooo@users.noreply.github.com>
2025-06-23 18:15:20 +0000

    deploy: ae29da30c7c69c18aaad2ec02057bc9ff98a4aea

---
+++ assets/back.B2-orv3D.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.BDZ9eHPV.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
---
+++ assets/index.BRXJGymZ.js
@@ -0,0 +1,1 @@
+const e={easeIn:[.18,.66,.05,.96]};export{e as c};
